3bbe3d07bf324f0a66273b2969a199b49b99e4f2
tmd45 <tmd45@otegami.jpn.org>
2015-11-03 22:58:47 +0900

    Add ommiauth-google settings

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+    Rails.application.secrets.google_client_id,
+    Rails.application.secrets.google_client_secret, {
+      name: 'google',
+      image_aspect_ratio: 'square'
+    }
+end
