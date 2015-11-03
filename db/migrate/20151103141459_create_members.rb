4e6308403c674b3fd90934f61a7f18bac7107f08
tmd45 <tmd45@otegami.jpn.org>
2015-11-03 23:24:17 +0900

    Fix members attributes in migration

--- db/migrate/20151103141459_create_members.rb
+++ db/migrate/20151103141459_create_members.rb
@@ -1,9 +1,9 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-      t.string :provider
-      t.string :uid
-      t.string :email
+      t.string :provider, null: false
+      t.string :uid,      null: false
+      t.string :email,    null: false
       t.string :given_name
       t.string :given_name_kana
       t.string :given_name_alphabet
@@ -15,5 +15,8 @@ class CreateMembers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :members, [ :provider, :uid ], unique: true, name: :index_provider_uid
+    add_index :members, :email, unique: true, name: :index_email
   end
 end
