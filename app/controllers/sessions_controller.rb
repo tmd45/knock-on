d8619c8f0700c0374b7e750174f2ec881a7b08f6
tmd45 <tmd45@otegami.jpn.org>
2015-11-06 01:37:23 +0900

    Add sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,7 @@
 class SessionsController < ApplicationController
+  def create
+    member = Member.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    session[:member_id] = member.id
+    redirect_to root_path, notice: 'ログインしました'
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'create' do
+    before do
+      auth_hash_google = File.read(File.join(Rails.root, 'spec', 'data', 'auth_hash_google.rb'))
+      request.env['omniauth.auth'] = OmniAuth::AuthHash.new(eval auth_hash_google)
+    end
 
+    subject { get :create, { provider: 'google' } }
+
+    it 'redirect to root path' do
+      expect(subject).to redirect_to root_path
+    end
+
+    it 'save member_id to session' do
+      subject
+      expect(session[:member_id]).to eq Member.last.id
+    end
+  end
 end
