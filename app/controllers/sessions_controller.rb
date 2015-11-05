d3da6e75f3280ad99a288761d5f9a0ab96cdab35
tmd45 <tmd45@otegami.jpn.org>
2015-11-06 02:05:34 +0900

    Fix login message to use translate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:member_id] = member.id
-    redirect_to root_path, notice: 'ログインしました'
+    redirect_to root_path, notice: t('omniauth.sessions.signed_in')
   end
 end
