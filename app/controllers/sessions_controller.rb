a6c43631427d878999d08c3099cacd7a7db77a7a
tmd45 <tmd45@otegami.jpn.org>
2015-11-07 15:28:40 +0900

    Fix locales key name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:member_id] = member.id
-    redirect_to root_path, notice: t('omniauth.sessions.signed_in')
+    redirect_to root_path, notice: t('omniauth.sessions.logged_in')
   end
 
   def destroy
     reset_session
-    redirect_to root_path, notice: t('omniauth.sessions.signed_out')
+    redirect_to root_path, notice: t('omniauth.sessions.logged_out')
   end
 end
