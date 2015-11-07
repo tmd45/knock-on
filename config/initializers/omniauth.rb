632d9724de80465b2220bcded51a423897a53d07
tmd45 <tmd45@otegami.jpn.org>
2015-11-07 15:38:06 +0900

    Add hosting domain limit setting as Google login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.secrets.google_client_id,
     Rails.application.secrets.google_client_secret, {
       name: 'google',
-      image_aspect_ratio: 'square'
+      image_aspect_ratio: 'square',
+      hd: Rails.application.secrets.google_hosted_domain,
     }
 end
