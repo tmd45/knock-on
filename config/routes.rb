b6981ca404473507b7101c475a4b972615e24dd8
tmd45 <tmd45@otegami.jpn.org>
2015-11-08 10:23:26 +0900

    Fix routing members#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'members/index'
-
   root to: 'welcome#index'
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  get 'members' => 'members#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
