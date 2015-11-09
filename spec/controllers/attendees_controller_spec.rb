4ce85384ce3f0c7c392b5d110d472b17c2d37238
tmd45 <tmd45@otegami.jpn.org>
2015-11-10 01:35:47 +0900

    Enable to work AttendeesController#create
    to notificate with Slack Incoming Webhook

--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -2,10 +2,64 @@ require 'rails_helper'
 
 RSpec.describe AttendeesController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe 'GET #create' do
+    before do
+      allow(Settings.slack).to receive(:incoming_webhook_url).
+        and_return('https://www.example.com/incoming_webhook_url')
+    end
+
+    context 'has member_id and the member has slack_identifier' do
+      let(:member) { create(:member, slack_identifier: 'taro') }
+
+      it 'notificate @mention to slack and returns http success' do
+        WebMock.stub_request(:post, 'https://www.example.com/incoming_webhook_url').with(
+          body: {
+            payload: "{\"text\":\"\\u003c@taro\\u003e 受付にお客様がお見えです。\"}"
+          }
+        ).to_return(status: 200)
+
+        get :create, { member_id: member.id }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'has member_id and the member has not slack_identifier' do
+      let(:member) { create(:member) }
+
+      it 'notificate @here mention and returns http success' do
+        WebMock.stub_request(:post, 'https://www.example.com/incoming_webhook_url').with(
+          body: {
+            payload: "{\"text\":\"\\u003c!here\\u003e 受付にお客様がお見えです。\"}"
+          }
+        ).to_return(status: 200)
+
+        get :create, { member_id: member.id }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'has not member_id' do
+      it 'notificate @here mention and returns http success' do
+        WebMock.stub_request(:post, 'https://www.example.com/incoming_webhook_url').with(
+          body: {
+            payload: "{\"text\":\"\\u003c!here\\u003e 受付にお客様がお見えです。\"}"
+          }
+        ).to_return(status: 200)
+
+        get :create
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'notificate failed' do
+      it 'returns Service Unavailable and respond json messages' do
+        WebMock.stub_request(:post, 'https://www.example.com/incoming_webhook_url').
+          to_return(status: 400)
+
+        get :create
+        expect(response).to have_http_status(503)
+        expect(JSON.parse(response.body)).to match( 'messages' => 'failed' )
+      end
     end
   end
 
