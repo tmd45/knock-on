56621bb5d51b69bd951f93557de3884dd6b95f2c
tmd45 <tmd45@otegami.jpn.org>
2015-11-16 23:10:07 +0900

    Fix spec for Admin::ProfileController

--- spec/controllers/admin/profile_controller_spec.rb
+++ spec/controllers/admin/profile_controller_spec.rb
@@ -3,10 +3,24 @@ require 'rails_helper'
 RSpec.describe Admin::ProfileController, type: :controller do
 
   describe 'GET #edit' do
-    it 'returns http success' do
-      get :edit
-      expect(response).to have_http_status(:success)
-      expect(assigns(:title)).to eq 'プロフィール編集'
+    context 'has not log-in' do
+      it 'redirect to root' do
+        get :edit
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'has logged-in' do
+      before do
+        member = create :member
+        session[:member_id] = member.id
+        get :edit
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+        expect(assigns(:title)).to eq 'プロフィール編集'
+      end
     end
   end
 
