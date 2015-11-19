d0b7be93c67da2ee479a74280af8d8fb145ef0fe
tmd45 <tmd45@otegami.jpn.org>
2015-11-19 23:49:14 +0900

    Fix migration create groups

--- db/migrate/20151119125252_create_groups.rb
+++ db/migrate/20151119125252_create_groups.rb
@@ -1,11 +1,13 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.string :name
-      t.string :name_alphabet
-      t.string :order
+      t.string :name, null: false
+      t.string :name_kana
+      t.integer :order, default: 0
 
       t.timestamps null: false
     end
+
+    add_index :groups, :order, name: :idx_groups_order
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103141459) do
+ActiveRecord::Schema.define(version: 20151119125252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",                   null: false
+    t.string   "name_kana"
+    t.integer  "order",      default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "groups", ["order"], name: "idx_groups_order", using: :btree
 
   create_table "members", force: :cascade do |t|
     t.string   "provider",             null: false
