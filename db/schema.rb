957b651f0451757b43ca7c70d0598ee3f9bb4f6f
tmd45 <tmd45@otegami.jpn.org>
2015-11-20 00:44:31 +0900

    Add column title to Member

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119145211) do
+ActiveRecord::Schema.define(version: 20151119154109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20151119145211) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.integer  "group_id"
+    t.string   "title"
+    t.string   "title_alphabet"
   end
 
   add_index "members", ["email"], name: "idx_members_email", unique: true, using: :btree
