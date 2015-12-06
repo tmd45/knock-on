a9eda08acfff74c7a3608ffae3fe78ebf75741ca
tmd45 <tmd45@otegami.jpn.org>
2015-12-06 16:13:58 +0900

    Add member edit in the admin page

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,16 +1,22 @@
 class Admin::MembersController < Admin::ApplicationController
   before_action :authenticate
   before_action :current_member
+  before_action :set_member, only: [ :edit, :update, :destroy ]
 
   # GET /admin/members
-  #
   def index
+    @title = '登録社員一覧'
     @members = Member.all.reorder(:id)
+  end
+
+  # GET /admin/members/:id/edit
+  def edit
+    @title = '社員情報編集'
   end
 
   private
 
-  def title
-    @title = '登録社員一覧'
+  def set_member
+    @member = Member.find(params[:id])
   end
 end
--- spec/controllers/admin/members_controller_spec.rb
+++ spec/controllers/admin/members_controller_spec.rb
@@ -2,4 +2,59 @@ require 'rails_helper'
 
 RSpec.describe Admin::MembersController, type: :controller do
 
+  shared_context 'member logged-in' do
+    before do
+      member = create :member
+      session[:member_id] = member.id
+    end
+  end
+
+  shared_examples_for 'Rejecting unknown member' do
+    context 'has not log-in' do
+      it 'redirect to root' do
+        subject
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'has logged-in unknown member' do
+      it 'redirect to root and clear session' do
+        session[:member_id] = 999
+        subject
+        expect(response).to redirect_to(root_path)
+        expect(session[:member_id]).to be_nil
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    subject { get :index }
+    it_behaves_like 'Rejecting unknown member'
+
+    context 'has logged-in' do
+      include_context 'member logged-in'
+
+      it 'returns http success' do
+        subject
+        expect(response).to have_http_status(:success)
+        expect(assigns(:title)).to eq '登録社員一覧'
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    subject { get :edit, { id: 1 } }
+    it_behaves_like 'Rejecting unknown member'
+
+    context 'has logged-in' do
+      include_context 'member logged-in'
+
+      it 'returns http success' do
+        subject
+        expect(response).to have_http_status(:success)
+        expect(assigns(:title)).to eq '社員情報編集'
+      end
+    end
+  end
+
 end
